372c21cfc48c4ddd3595401567b5983373339e98
kevinQTruong <kevinQTruong@users.noreply.github.com>
2023-06-20 22:07:43 +1000

    add no semi rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    '@typescript-eslint/semi': ['error', 'never'],
+    "semi": "off"
   },
 };
 
